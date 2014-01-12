eb94706ec04e2294f3f1e0d8e32658fac466ba1a
q13 <qsimon13@gmail.com>
2014-01-12 23:37:32 +0800

    update

--- fxiaoke/advance2.js
+++ fxiaoke/advance2.js
@@ -7,23 +7,15 @@ var webPage=require('webpage');
 var page = webPage.create();
 var childWinIndex= 0,
     totalWinCounts=0;
+var tempPageHeaders={};
 page.customHeaders = {
     "User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:26.0) Gecko/20100101 Firefox/26.0 FirePHP/0.7.4"
 };
 page.onResourceRequested = function (request) {
     //console.log('Request ' + JSON.stringify(request, undefined, 4));
-    if(request.url.search('w.gif')!=-1){
-        //console.log('request ' + JSON.stringify(request, undefined, 4));
-    }
 };
 page.onResourceReceived = function (response) {
-    //console.log('Request ' + JSON.stringify(response, undefined, 4));
-    page.cookies.forEach(function(cookieData){
-        phantom.addCookie(cookieData);
-    });
-    if(response.url.search('w.gif')!=-1){
-        //console.log('Receive ' + JSON.stringify(response, undefined, 4));
-    }
+    //console.log('Receive ' + JSON.stringify(response, undefined, 4));
 };
 page.onResourceReceived = function (response) {
     //console.log('Receive ' + JSON.stringify(response, undefined, 4));
@@ -32,32 +24,43 @@ page.onConsoleMessage = function(msg, lineNum, sourceId) {
     //console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
 };
 page.onCallback = function(adLinks) {
+    var tempRecordIndex=0;
     totalWinCounts=adLinks.length;
     page.render('temp/search.png');
+    page.cookies.forEach(function(cookieData){
+        phantom.addCookie(cookieData);
+    });
+    adLinks.forEach(function(link){
+        var tempPage = webPage.create();
+        tempPage.customHeaders=tempPageHeaders;
+        tempPage.open(link,function(){
+            tempRecordIndex++;
+            console.log(link);
+            tempPage.release();
+            if(tempRecordIndex==adLinks.length){
+                phantom.exit();
+            }
+        });
+        tempPage.onResourceRequested = function (request) {
+            //console.log('Request ' + JSON.stringify(request, undefined, 4));
+        };
+        tempPage.onResourceReceived = function (response) {
+            tempPage.cookies.forEach(function(cookieData){
+                phantom.addCookie(cookieData);
+            });
+        };
+    });
     if(adLinks.length==0){
         console.log('nothing founded');
         phantom.exit();
     }
 };
 page.onPageCreated = function(newPage) {
-    //console.log(JSON.stringify(phantom.cookies, undefined, 4));
-    //phantom.cookies.forEach(function(cookieData){
-
-        //newPage.addCookie(cookieData);
-        //console.log(JSON.stringify(newPage.cookies, undefined, 4));
-    //});
     newPage.onResourceRequested = function (request) {
-        if(request.url.search('baidu.php')!=-1){
-            //console.log(JSON.stringify(newPage.cookies, undefined, 4));
-        }
-        //console.log('Request ' + JSON.stringify(request, undefined, 4));
-    };
-    newPage.onLoadStarted=function(){
-        //console.log(JSON.stringify(newPage.cookies, undefined, 4));
-        console.log('link load start');
+        console.log('Request ' + JSON.stringify(request, undefined, 4));
     };
     newPage.onLoadFinished = function(status) {
-        //console.log(JSON.stringify(newPage.cookies, undefined, 4));
+        console.log(JSON.stringify(newPage.cookies));
         //console.log(newPage.url);
         childWinIndex++;
         newPage.render('temp/page-'+childWinIndex+'.png');
@@ -78,7 +81,7 @@ page.onLoadFinished = function(status) {
             page.evaluate(function() {
                 var store=[],
                     linksEl=$('#content_left table.result').eq(0).prevAll('.EC_ppim_top').find('.EC_title');
-                    //linksEl=$('.EC_ppim_top').eq(0).find('.EC_title');
+                //linksEl=$('.EC_ppim_top').eq(0).find('.EC_title');
                 linksEl.each(function(){
                     var meEl=$(this);
                     var meOffset= meEl.offset(),
@@ -91,7 +94,6 @@ page.onLoadFinished = function(status) {
                 });
                 setTimeout(function(){
                     linksEl.each(function(){
-                        var that=this;
                         var meEl=$(this);
                         var meOffset= meEl.offset(),
                             meScrollTop=meEl.scrollTop(),
@@ -101,17 +103,12 @@ page.onLoadFinished = function(status) {
                         meOffset.top=meOffset.top+5;
                         meOffset.left=meOffset.left+30;
                         evtMd = document.createEvent('MouseEvents');
-                        evtMd.initMouseEvent("mousedown", true, true,window,1,meOffset.left-meScrollLeft,meOffset.top-meScrollTop,meOffset.left,meOffset.top,false,false,false,false,0,null); //模拟mousedown
+                        evtMd.initMouseEvent("mousedown", true, true,window,1,meOffset.left-meScrollLeft,meOffset.top-meScrollTop,meOffset.left,meOffset.top,false,false,false,false,2,null); //模拟mousedown
                         evtMu = document.createEvent('MouseEvents');
-                        evtMu.initMouseEvent("mouseup", true, true,window,1,meOffset.left-meScrollLeft,meOffset.top-meScrollTop,meOffset.left,meOffset.top,false,false,false,false,0,null); //模拟mouseup
+                        evtMu.initMouseEvent("mouseup", true, true,window,1,meOffset.left-meScrollLeft,meOffset.top-meScrollTop,meOffset.left,meOffset.top,false,false,false,false,2,null); //模拟mouseup
                         this.dispatchEvent(evtMd);
                         this.dispatchEvent(evtMu);
-                        setTimeout(function(){ //等待w.gif返回
-                            this.open(that.href);
-                        },5000);
                         //window.open(this.href);
-                        //触发点击事件
-                        //$(this).trigger('click');
                         store.push(this.href);
                     });
                     this.callPhantom(store);
@@ -126,7 +123,7 @@ page.open('http://www.baidu.com', function(status) {
     if ( status === "success" ) {
         page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js", function() {
             page.evaluate(function() {
-                $('#kw').val('小米');
+                $('#kw').val('我买菜');
                 $('#su').click();
             });
             //phantom.exit();
